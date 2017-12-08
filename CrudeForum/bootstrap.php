1e02238fe4bcad6c3947069ddfced8fcc3b443a2
Koala Yeung <koalay@gmail.com>
2017-12-08 17:27:15 +0800

    add composer autoload for Core
    * update Core class with misc code.
    * added composer.json with proper psr-4 autoload config.
    * added autoload to bootstrap.php

--- CrudeForum/bootstrap.php
+++ CrudeForum/bootstrap.php
@@ -7,3 +7,10 @@ error_reporting(E_ALL);
 // common bootstrap code
 require_once __DIR__ . '/configuration.php';
 require_once __DIR__ . '/misc.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
+$forum = new \ywsing\CrudeForum\Core([
+    'logDirectory' => CRUDE_DIR_LOGS,
+    'dataDirectory' => CRUDE_DIR_DATA,
+    'administrator' => CRUDE_ADMIN,
+]);
--- CrudeForum/configuration.example.php
+++ CrudeForum/configuration.example.php
@@ -4,13 +4,14 @@ if (!defined('CRUDE_DIR')) {
     define('CRUDE_DIR', __DIR__);
     define('CRUDE_DIR_DATA', CRUDE_DIR . '/data/forumdata_utf8/');
     define('CRUDE_DIR_LOGS', CRUDE_DIR . '/data/logs');
+    define('CRUDE_ADMIN', '');
 }
 
 $logDirectory = CRUDE_DIR_LOGS;
 $dataDirectory = CRUDE_DIR_DATA;
 $beginFormat = "";//"<body bgcolor=#111111 text=#777777 link=#AAAA00 vlink=#777777><font face=courier>";
 $endFormat = "";//"</font></body>";
-$administrator = "";
+$administrator = CRUDE_ADMIN;
 date_default_timezone_set ('Asia/Hong_Kong');
 
 ?>
