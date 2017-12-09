768ca4e8751f41a67ff3357822bbdb1766913a99
Koala Yeung <koalay@gmail.com>
2017-12-10 04:50:28 +0800

    improved forum.php with Paged
    * rewrite paging logic in forum.php with Paged.
    * fix hard code 100 post per page into common variable.

--- forum.php
+++ forum.php
@@ -1,9 +1,12 @@
 <?php
 
 include __DIR__ . '/CrudeForum/bootstrap.php';
+
+use ywsing\CrudeForum\Paged;
+
 $page = (int) getenv("QUERY_STRING");
 if ($page < 0) $page = 0;
-$pagePerPage = 100;
+$postPerPage = 100;
 
 $forum->log("forum?" . $page);
 
@@ -18,7 +21,7 @@ $forum->log("forum?" . $page);
 
 <title>Wandering and wonderings</title>
 <table width=100%><tr><td><div class=buttons><a href=sayForm.php>發言</a>&nbsp;&nbsp;<a href=
-forum.php?<?php if($page > 100) print $page - 100; else print '0'; ?>
+forum.php?<?php if($page > $postPerPage) print $page - $postPerPage; else print '0'; ?>
 >上一頁</a>&nbsp;&nbsp;<a href=forum.php>首頁</a>&nbsp;&nbsp;<a href=index.html>主頁</a></div></td>
 <td align=right><div class=text>心意就如密友，長路裏相伴漫遊。</div></td></tr></table><br>
 
@@ -26,22 +29,19 @@ forum.php?<?php if($page > 100) print $page - 100; else print '0'; ?>
 
 <?php
 $lock = $forum->getLock();
-$index = $forum->getIndex();
-foreach ($index as $key => $postSummary) {
-    if ($key >= $page) {
-        print str_repeat("　 ", $postSummary->level);
-        print "<a href=\"read.php?{$postSummary->id}\">{$postSummary->title}</a>";
-        print ($postSummary->level = 0) ? "　　　 -- " : "　　-- ";
-        print $postSummary->author . " at " . $postSummary->time . "<br>";
-    }
-    if ($key >= ($page + $pagePerPage - 1)) break;
+$index = new Paged($forum->getIndex(), $page, $postPerPage);
+foreach ($index as $postSummary) {
+    print str_repeat("　 ", $postSummary->level);
+    print "<a href=\"read.php?{$postSummary->id}\">{$postSummary->title}</a>";
+    print ($postSummary->level = 0) ? "　　　 -- " : "　　-- ";
+    print $postSummary->author . " at " . $postSummary->time . "<br>";
 }
 fclose ($lock);
 ?>
 
 </div><br><br><hr><br>
 <table width=100%><tr align=top><td><div class=buttons><a href=sayForm.php>發言</a>&nbsp;&nbsp;<a href=
-forum.php?<?php print ((int) $page) + 100; ?>
+forum.php?<?php print ((int) $page) + $postPerPage; ?>
 >下一頁</a>&nbsp;&nbsp;<a href=forum.php>首頁</a>&nbsp;&nbsp;<a href=../index.html>回主頁</a></div></td><td align=right>漫長漫長夜間，我伴我閒談；漫長漫長夜晚，從未覺是冷。</td></tr></table>
 
 <?php print $endFormat; ?>
