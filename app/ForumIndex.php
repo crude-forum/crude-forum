dd051de3ed797d26ac27c0f8e5b5f9b13bae01c1
Koala Yeung <koalay@gmail.com>
2017-12-12 16:31:06 +0800

    trim line before parsing as PostSummary
    * fix issue with strange empty lines in the index.

--- app/ForumIndex.php
+++ app/ForumIndex.php
@@ -7,7 +7,7 @@ use \ywsing\CrudeForum\Iterator\Wrapper;
 class ForumIndex extends Wrapper implements \Iterator {
     public function current() {
         return PostSummary::fromIndexLine(
-            $this->iter->current(),
+            trim($this->iter->current()),
             $this->iter->key());
     }
 }
