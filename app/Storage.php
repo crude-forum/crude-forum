c4650e1930562747d8121c9dc9e5c8e2893589de
Koala Yeung <koalay@gmail.com>
2017-12-12 11:15:57 +0800

    improve Storage error handling
    * update writePost and appendIndex to better handle error. Throw
      exception and do proper recovery on error.

--- app/Storage.php
+++ app/Storage.php
@@ -92,65 +92,96 @@ class Storage {
         return new Post($title, $body, $noAutoBr);
     }
 
-    public function writePost(int $postID, Post $post) {
-
+    public function writePost(int $postID, Post $post): bool {
         // determine data folder for the post
-        $subdir = floor((int) $postID / 1000) . '/';
-        if (!is_dir($this->dataDirectory . $subdir)) {
-            mkdir($this->dataDirectory . $subdir);
-            chmod($this->dataDirectory . $subdir, 0777);
+        $subdir = floor((int) $postID / 1000);
+        if (!is_dir($this->dataDirectory . '/' . $subdir)) {
+            if (mkdir($this->dataDirectory . '/' . $subdir) === FALSE) {
+                throw new \Exception('failed to create subdirectory for the post');
+                return FALSE;
+            }
+            if (chmod($this->dataDirectory . '/' . $subdir, 0777) === FALSE) {
+                throw new \Exception('failed to chmod post subdirectory');
+                return FALSE;
+            }
         }
-
-        // determine post file fullpath
-        $fh = fopen($this->dataDirectory . '/' . $subdir . $postID, "w+");
-        fputs($fh, sprintf("%s\n\n%s", $post->title, $post->body));
-        fclose($fh);
+        $fh = fopen($this->dataDirectory . '/' . $subdir  . '/' . $postID, "w+");
+        if (!is_resource($fh)) {
+            throw new \Exception('failed to open post file to write');
+            return FALSE;
+        }
+        if (!fputs($fh, sprintf("%s\n\n%s", $post->title, $post->body))) {
+            throw new \Exception('failed to write to post file');
+            return FALSE;
+        }
+        if (!fclose($fh)) {
+            throw new \Exception('failed to close post file');
+            return FALSE;
+        }
+        return TRUE;
     }
 
-    public function appendIndex(PostSummary $postSummary, $parentID=FALSE) {
-        rename($this->dataDirectory . '/index', $this->dataDirectory . '/index.old');
+    public function appendIndex(PostSummary $postSummary, $parentID=FALSE): bool {
 
-        $fh_old = fopen($this->dataDirectory . '/index.old', 'r+');
-        if (!is_resource($fh_old)) {
-            throw new \Exception('unable to open index.old for read');
-            return FALSE;
-        }
-        $oldIndex = new ForumIndex(new FileObject($fh_old));
-
-        $fh = fopen($this->dataDirectory . '/index', 'w+');
-        if (!is_resource($fh)) {
-            throw new \Exception('unable to open index for write');
+        // swap the index out and prepare to rewrite index
+        if (!rename($this->dataDirectory . '/index', $this->dataDirectory . '/index.old')) {
+            throw new \Exception('unable to rename index as index.old');
             return FALSE;
         }
 
-        // if this is not a reply
-        if ($parentID === FALSE) {
-            fputs($fh, $postSummary->toIndexLine());
-            foreach ($oldIndex as $oldPostSummary) {
-                fputs($fh, $oldPostSummary->toIndexLine());
+        try {
+            $fh_old = fopen($this->dataDirectory . '/index.old', 'r+');
+            if (!is_resource($fh_old)) {
+                throw new \Exception('unable to open index.old for read');
+                return FALSE;
             }
-            unset($index);
+            $oldIndex = new ForumIndex(new FileObject($fh_old));
+
+            $fh = fopen($this->dataDirectory . '/index', 'w+');
+            if (!is_resource($fh)) {
+                throw new \Exception('unable to open index for write');
+                return FALSE;
+            }
+
+            // if this is not a reply
+            if ($parentID === FALSE) {
+                fputs($fh, $postSummary->toIndexLine());
+                foreach ($oldIndex as $oldPostSummary) {
+                    fputs($fh, $oldPostSummary->toIndexLine());
+                }
+                unset($index);
+                unlink($this->dataDirectory . '/index.old');
+                return TRUE;
+            }
+
+            // if this is a reply
+            $parentID = (int) $parentID;
+            foreach ($oldIndex as $pos => $oldPostSummary) {
+                fputs($fh, $oldPostSummary->toIndexLine());
+                // append to below parent
+                if ($oldPostSummary->id == $parentID) {
+                    $postSummary->level = $oldPostSummary->level + 1;
+                    $postSummary->pos = $pos + 1;
+                    fputs($fh, $postSummary->toIndexLine());
+                }
+            }
+            fclose($fh);
+
+            // close and remove old index
+            unset($oldIndex);
             unlink($this->dataDirectory . '/index.old');
             return TRUE;
-        }
 
-        // if this is a reply
-        $parentID = (int) $parentID;
-        foreach ($oldIndex as $pos => $oldPostSummary) {
-            fputs($fh, $oldPostSummary->toIndexLine());
-            // append to below parent
-            if ($oldPostSummary->id == $parentID) {
-                $postSummary->level = $oldPostSummary->level + 1;
-                $postSummary->pos = $pos + 1;
-                fputs($fh, $postSummary->toIndexLine());
+        } catch (\Exception $e) {
+            // restore index
+            if (!rename($this->dataDirectory . '/index.old', $this->dataDirectory . '/index')) {
+                throw new \Exception('unable to restore index.old as index');
+                return FALSE;
             }
+            throw $e;
+            return FALSE;
         }
-        fclose($fh);
-
-        // close and remove old index
-        unset($oldIndex);
-        unlink($this->dataDirectory . '/index.old');
-        return TRUE;
+        return FALSE;
     }
 
     public function getLock() {
